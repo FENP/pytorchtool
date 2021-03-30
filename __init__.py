c0e5b17921eff20619434e9263f4dae701ffc825
fenp <32334296+FENP@users.noreply.github.com>
2021-03-30 16:51:07 +0800

    添加了模型切分模块

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 from pytorchtool.profile import Profile
 from pytorchtool.prepare import save_model
+from pytorchtool.surgery import Surgery
 
 name = "pytorchtool"
 
-__all__ = ["Profile", "save_model"]
-__version__ = "0.0.1"
+__all__ = ["Profile", "save_model", "Surgery"]
+__version__ = "0.1.0"
 
---
+++ surgery.py
@@ -0,0 +1,91 @@
+# coding=utf-8
+import sys
+import torch
+import functools
+from collections import defaultdict, namedtuple
+
+# name:模块名；module:模块
+Trace = namedtuple("Trace", ["name", "module"])
+
+def walk_modules(module, name="", depth=-1):
+    """生成器。根据depth遍历pytorch模块，生成Trace元组"""
+
+    child_list = list(module.named_children())
+    '''
+    遍历到叶子结点或depth指定的深度时返回当前模块元组；
+    否则继续向下遍历
+    '''
+    if depth == 0 or len(child_list) == 0:
+        yield Trace(name, module)
+    else:
+        for child in child_list:
+            yield from walk_modules(child[1], child[0] if name=="" else name + "." + child[0], depth - 1)
+
+
+class Surgery(object):
+    """对PyTorch模型进行处理，方便进行模型切分"""
+
+    def __init__(self, model, mode, use_cuda=False, depth=-1):
+        """
+        参数：
+            model：初始化后的DNN模型
+            mode：切分模式 0：客户端模式（执行中间层并存储输出）、2：客户端模式（根据中间层输出继续执行，得到最终结果）
+        """
+        self._model = model
+        self._mode = mode
+        self._use_cuda = use_cuda
+        self._depth = depth
+        
+        self._layerState = None
+
+        # 存储初始forwards
+        self._forwards = {}
+
+        # 存储中间层输出
+        self._middleResult = defaultdict(list)
+
+        # 逐层修改forwards
+        self.traces = tuple(map(self._hook_trace, walk_modules(self._model, depth=self._depth)))
+        return self
+
+    def getMiddleResult(self):
+        return self._middleResult
+
+    def setMiddleResult(self, middleResult):
+        if mode == 2:
+            self._middleResult = middleResult
+
+    def clearMiddleResult(self):
+        if mode == 0:
+            self._middleResult.clear()
+
+    def setLayerState(self, layerState):
+        self._layerState = layerState
+
+    def __call__(self, *args, **kwargs):
+        return self._model(*args, **kwargs)
+
+    def _hook_trace(self, trace):
+        [name, module] = trace
+        _forward = module.forward
+        self._forwards[name] = _forward
+
+        @functools.wraps(_forward)
+        def wrap_forward(*args, **kwargs):
+            if self._layerState[name] != mode:
+                # 非中间输出层直接返回原始数据
+                if self._layerState[name] != 1:
+                    return args[0]
+                # 服务端模式获取层输出并返回
+                else if mode == 2:
+                    return self._middleResult[name]
+
+            output = _forward(*args, **kwargs)
+            
+            # 客户端模型下需要存储中间层输出
+            if mode == 0 and self._layerState[name] == 1:
+                self._middleResult[name] = output
+            return output
+
+        module.forward = wrap_forward
+        return trace
